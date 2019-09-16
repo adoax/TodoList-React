44a2c0b03db3e656ae2773c9d3fa022432663526
seguinard <anthony.seguinard@live.fr>
2019-09-16 17:02:37 +0200

    Suppresion code inutilisé

--- src/Todo/TodoApp.js
+++ src/Todo/TodoApp.js
@@ -6,52 +6,58 @@ class TodoApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = { items: [], text: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.ladate = new Date();
     }
+    onChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    AddTodo (e) {
+        e.preventDefault();
+        this.setState({
+            text: '',
+            done: false,
+            items: [...this.state.items, this.state.text]
+        });
+    }
+
+    RemoveTodo (item) {
+        const array =   this.state.items
+        const index = array.indexOf(item);
+        array.splice(index, 1)
+        this.setState({ items: array })
+    }
+
+
+    renderTodo (e) {
+        return this.state.items.map((item) => {
+            return (
+                    <li key={item} className={this.todoClass}>{item} <button className="badge badge-danger" onClick={this.RemoveTodo.bind(this, item)}> X</button>
+                    </li>
+            )
+       });
+    }
+
     render() {
         return (
             <div>
                 <h1 className="text-center p-4"> Todo list</h1>
                 <h3>A faire</h3>
-                <TodoList items={this.state.items} />
-                <form onSubmit={this.handleSubmit}>
+                <ul> 
+                    {this.renderTodo()}
+                </ul>
+                <form >
                     <div className="form-group">
                         <label>REnseigner une tache a effectuer</label>
-                        <input type="text" className="form-control" onChange={this.handleChange} value={this.state.text} />
+                        <input type="text" className="form-control" onChange={this.onChange.bind(this)}  value={this.state.text}/>
                     </div>
-                    <button className="btn btn-primary">
-                        ajouter {this.state.items.length + 1}
-                    </button>
+                    <button onClick={this.AddTodo.bind(this)} className="btn btn-primary">Envoyer</button>
                 </form>
             </div>
         );
     }
 
-    handleChange(e) {
-        this.setState({ text: e.target.value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        if (!this.state.text.length) {
-            return;
-        }
-        const newItem = {
-            text: this.state.text,
-            id: (this.ladate.getDate()
-            +'/'+(this.ladate.getMonth()+1)
-            +'/'+this.ladate.getFullYear()
-            +'-' +this.ladate.getHours()
-            +'h' + this.ladate.getMinutes())
-        };
-        this.setState(state => ({
-            items: state.items.concat(newItem),
-            text: ''
-        }));
-    }
-    
 
 }
 
